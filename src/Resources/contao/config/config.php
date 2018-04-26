246912004e8d0e4d770f50b28a19f6ea4a85e474
Stephan Preßl <mail@stephanpressl.at>
2018-04-26 19:28:02 +0200

    add contao config file

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,13 +1,10 @@
 <?php
 /*******************************************************************
- *
- * (c) 2017 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * (c) 2018 Stephan Preßl, www.prestep.at <development@prestep.at>
  * All rights reserved
- *
  * Modification, distribution or any other action on or with
  * this file is permitted unless explicitly granted by IIDO
  * www.iido.at <development@iido.at>
- *
  *******************************************************************/
 
 
@@ -17,6 +14,8 @@ $assetsPath = \IIDO\ShopBundle\Config\BundleConfig::getBundlePath(true, false);
 $ns         = $namespace . '\\' . $subNamespace;
 $modPrefix  = ucfirst( $subName );
 
+$listenerName = \IIDO\ShopBundle\Config\BundleConfig::getListenerName( true );
+
 
 // Load icon in Contao 4.2+ backend
 if( 'BE' === TL_MODE )
@@ -25,8 +24,8 @@ if( 'BE' === TL_MODE )
 }
 elseif( 'FE' === TL_MODE )
 {
-    $GLOBALS['TL_JAVASCRIPT'][] = \IIDO\ShopBundle\Config\BundleConfig::getBundlePath( true ) . '/javascript/IIDO.Shop.js|static';
-    $GLOBALS['TL_JAVASCRIPT']['cookie'] = \IIDO\BasicBundle\Config\BundleConfig::getBundlePath( true ) . '/javascript/jquery/jquery.cookie.min.js|static';
+    $GLOBALS['TL_JAVASCRIPT']['iido_shop']  = \IIDO\ShopBundle\Config\BundleConfig::getBundlePath( true ) . '/javascript/IIDO.Shop.js|static';
+    $GLOBALS['TL_JAVASCRIPT']['cookie']     = \IIDO\BasicBundle\Config\BundleConfig::getBundlePath( true ) . '/javascript/cookie.min.js|static';
 }
 
 
@@ -37,7 +36,7 @@ elseif( 'FE' === TL_MODE )
 
 $GLOBALS['IIDO']['SHOP']['API'] = array
 (
-    'weclapp'
+    'weclapp'   => array('token', 'tenant')
 );
 
 
@@ -70,6 +69,12 @@ array_insert($GLOBALS['BE_MOD'], 3, array
             'tables'        => array($tablePrefix . 'api')
         ),
 
+        $prefix . $modPrefix . 'Settings' => array
+        (
+            'tables'        => array($tablePrefix . 'payment', $tablePrefix . 'shipping', $tablePrefix . 'configuration'),
+            'callback'      => $ns . '\BackendModule\ShopSettingsModule'
+        ),
+
    ),
 
 ));
@@ -81,9 +86,21 @@ array_insert($GLOBALS['BE_MOD'], 3, array
  */
 
 $GLOBALS['TL_CTE']['iido_shop']['iido_shop_productList']        = 'IIDO\ShopBundle\ContentElement\ProductListElement';
+$GLOBALS['TL_CTE']['iido_shop']['iido_shop_productDetails']     = 'IIDO\ShopBundle\ContentElement\ProductDetailsElement';
 $GLOBALS['TL_CTE']['iido_shop']['iido_shop_configurator']       = 'IIDO\ShopBundle\ContentElement\ConfiguratorElement';
 $GLOBALS['TL_CTE']['iido_shop']['iido_shop_cart']               = 'IIDO\ShopBundle\ContentElement\ShopCartElement';
 $GLOBALS['TL_CTE']['iido_shop']['iido_shop_watchlist']          = 'IIDO\ShopBundle\ContentElement\WatchlistElement';
+$GLOBALS['TL_CTE']['iido_shop']['iido_shop_checkout']           = 'IIDO\ShopBundle\ContentElement\ShopCheckOutElement';
+$GLOBALS['TL_CTE']['iido_shop']['iido_shop_orderOverview']      = 'IIDO\ShopBundle\ContentElement\OrderOverviewElement';
+
+
+
+/**
+ * Hooks
+ */
+
+$GLOBALS['TL_HOOKS']['outputBackendTemplate'][]             = array($listenerName . '.backend_template', 'outputShopBackendTemplate');
+//$GLOBALS['TL_HOOKS']['parseBackendTemplate'][]              = array($listenerName . '.backend_template', 'parseShopBackendTemplate');
 
 
 
@@ -106,3 +123,51 @@ $GLOBALS['TL_PERMISSIONS'][] = 'iidoShopProductCategories_default';
 $GLOBALS['TL_MODELS']['tl_iido_shop_product']           = 'IIDO\ShopBundle\Model\IidoShopProductModel';
 $GLOBALS['TL_MODELS']['tl_iido_shop_product_category']  = 'IIDO\ShopBundle\Model\IidoShopProductCategoryModel';
 $GLOBALS['TL_MODELS']['tl_iido_shop_archive']           = 'IIDO\ShopBundle\Model\IidoShopArchiveModel';
+$GLOBALS['TL_MODELS']['tl_iido_shop_payment']           = 'IIDO\ShopBundle\Model\IidoShopPaymentModel';
+$GLOBALS['TL_MODELS']['tl_iido_shop_shipping']          = 'IIDO\ShopBundle\Model\IidoShopShippingModel';
+
+
+
+/**
+ * Ajax Actions
+ */
+
+$GLOBALS['AJAX']['iidoShop'] = array
+(
+    'actions' => array
+    (
+        // run in content element: shop cart
+        'getPrice' => array
+        (
+            'arguments'     => array('itemNumber', 'productName', 'quantity'),
+            'optional'      => array()
+        ),
+
+
+        'renderPrice' => array
+        (
+            'arguments'     => array('price', 'useDecimals'),
+            'optional'      => array('useDecimals')
+        ),
+
+
+        'getAddToCartMessage' => array
+        (
+            'arguments'     => array('productName')
+        ),
+        'getConfiguratorAddToCartMessage' => array
+        (
+            'arguments'     => array('productName')
+        ),
+
+
+        'getAddToWatchlistMessage' => array
+        (
+            'arguments'     => array('productName')
+        ),
+        'getConfiguratorAddToWatchlistMessage' => array
+        (
+            'arguments'     => array('productName')
+        )
+    ),
+);
