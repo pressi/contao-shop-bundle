15d832d34cb79b5fae6d4d7523ea03eed2259d9c
Stephan Preßl <mail@stephanpressl.at>
2018-10-13 11:00:00 +0200

    add new model classes

---
+++ src/Model/IidoShopStatisticOrderModel.php
@@ -0,0 +1,26 @@
+<?php
+/*******************************************************************
+ * (c) 2018 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *******************************************************************/
+
+namespace IIDO\ShopBundle\Model;
+
+
+/**
+ *
+ *
+ */
+class IidoShopStatisticOrderModel extends \Model
+{
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    protected static $strTable = 'tl_iido_shop_statistic_order';
+}
